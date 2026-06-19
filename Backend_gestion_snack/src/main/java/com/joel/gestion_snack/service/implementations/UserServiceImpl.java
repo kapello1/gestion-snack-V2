ca6faa8ec3a38e2c49cd24f9443d06da6b34776c
kapello1 <ton-email-tiegnijoel@yahoo.fr>
2026-06-19 13:09:07 +0200

    fix: restaurer import UUID dans UserServiceImpl

--- Backend_gestion_snack/src/main/java/com/joel/gestion_snack/service/implementations/UserServiceImpl.java
+++ Backend_gestion_snack/src/main/java/com/joel/gestion_snack/service/implementations/UserServiceImpl.java
@@ -33,6 +33,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Random;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 /**
